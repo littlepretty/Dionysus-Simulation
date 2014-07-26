089309a6e37671008ddee3ed7e3827612e6dd230
littlepretty <littlepretty881203@gmail.com>
2014-07-26 22:14:32 +0800

    Use unit test framework to test

--- python-graph-dionysus-extension/core/dionysus/test_ddg.py
+++ python-graph-dionysus-extension/core/dionysus/test_ddg.py
@@ -7,14 +7,16 @@ from dionysus_dependency_graph import dionysus_dependency_graph
 from dionysus_algorithms import *
 from pygraph.readwrite.dot import write
 
+import unittest
+
 def create_FR_case_dependency_graph():
 	gr = dionysus_dependency_graph()
-	# add operation
-	gr.add_operation_node("s8")
+
+	gr.add_operation_node("s8", 1, True, "del_tunnel")
 	gr.add_operation_node("s7")
-	gr.add_operation_node("s6")
+	gr.add_operation_node("s6", 1, True, "chg_weight")
 	gr.add_operation_node("s5_1")
-	gr.add_operation_node("s5_2")
+	gr.add_operation_node("s5_2", 1, True, "del_tunnel")
 
 	gr.add_operation_node("s4")
 	gr.add_operation_node("s3")
@@ -39,6 +41,8 @@ def create_FR_case_dependency_graph():
 	gr.add_edge(("s3-s4", "s1"), 5)
 	gr.add_edge(("s4-s5", "s4"), 5)
 	gr.add_edge(("s1", "s1-s8"), 5)
+
+
 	gr.add_edge(("s4", "s4-s7"), 5)
 	gr.add_edge(("s4-s7", "s1"), 5)
 	gr.add_edge(("s2", "s4-s5"), 5)
@@ -49,42 +53,75 @@ def create_FR_case_dependency_graph():
 
 	return gr
 
-def test_dependency_graph(graph):
+class TestDDGFunctions(unittest.TestCase):
+	"""use unittest framework testing dionysus_dependency_graph"""
+	def setUp(self):
+		self.graph = create_FR_case_dependency_graph()
 
-	graph.print_all()
+	def test_incidents(self):
+		self.assertItemsEqual(self.graph.incidents("s8"), ["s5_1", "s7"])
 
-	print "*** s8's incidents(parents) ***"
-	for each in graph.incidents("s8"):
-		print each
+	def test_neighbors(self):
+		self.assertItemsEqual(self.graph.neighbors("s8"), ["s6", "s5_2", "s6-s5"])
 
-	print "*** s8's neighbors(children) ***"
-	for each in graph.neighbors("s8"):
-		print each
+	def test_is_add_tunnel_operation(self):
+		self.assertFalse(self.graph.is_add_tunnel_operation("s8"))
+		self.assertTrue(self.graph.is_add_tunnel_operation("s7"))
 
-	cpl_list = critial_path_length_list(graph)
+	def test_is_del_tunnel_operation(self):
+		self.assertTrue(self.graph.is_del_tunnel_operation("s8"))
+		self.assertFalse(self.graph.is_del_tunnel_operation("s3"))
 
-	print "*** node in its cpl's decreasing order ***"
-	for each in cpl_list:
-		print "node & its cpl: " + str(each)
-		graph.set_scheduled(each[0])
+	def test_is_chg_weight_operation(self):
+		self.assertTrue(self.graph.is_chg_weight_operation("s6"))
+		self.assertFalse(self.graph.is_chg_weight_operation("s3"))
 
-	graph.print_all()
+	def test_has_operation_parents(self):
+		self.assertTrue(self.graph.has_operation_parents("s1-s8"))
+		self.assertFalse(self.graph.has_operation_parents("s1"))
 
-	graph.del_node("s8")
+	def test_has_no_parents(self):
+		self.assertTrue(self.graph.has_no_parents("s7"))
+		self.assertFalse(self.graph.has_no_parents("s4"))
 
-	graph.print_all()
+	def test_operation_nodes(self):
+		self.assertItemsEqual(self.graph.operation_nodes(), ["s1", "s2", "s3", "s4", "s5_1", "s5_2", "s6", "s7", "s8"])
 
-	print graph.edge_connecting_nodes("s4-s7", "s1")
-	print graph.edge_weight_on_nodes("s4-s7", "s1")
+	def test_finished_operation_nodes(self):
+		self.assertItemsEqual(self.graph.finished_operation_nodes(), ["s5_2", "s6", "s8"])
 
+	def test_resource_nodes(self):
+		self.assertItemsEqual(self.graph.resource_nodes(), ["s1-s8", "s4-s7", "s3-s4", "s4-s5", "s6-s5"])
 
-def main():
-	graph = create_FR_case_dependency_graph()
-	test_dependency_graph(graph)
+	def test_print_all(self):
+		self.graph.print_all()
+
+	def test_edge_connecting_nodes(self):
+		print "\n" + str(self.graph.edge_connecting_nodes("s4-s7", "s1")) + "\n"
+		print "\n" + str(self.graph.edge_connecting_nodes("s7", "s2")) + "\n"
+
+	def test_edge_weight_on_nodes(self):
+		print "\n" + str(self.graph.edge_weight_on_nodes("s4-s7", "s1")) + "\n"
+		print "\n" + str(self.graph.edge_weight_on_nodes("s7", "s1")) + "\n"
+
+	def test_critial_path_length_list(self):
+		# test CPL calculating and sorting
+		cpl_list = critial_path_length_list(self.graph)
+
+		print "\n *** node in its cpl's decreasing order ***"
+		for each in cpl_list:
+			print "node & its cpl: " + str(each)
+		print "*******************************************\n"
+
+		cpl_list = critial_path_length_list(dionysus_dependency_graph())
+		self.assertEqual(cpl_list, [])
+
+	def test_schedule_graph(self):
+		pass
 
 
 if __name__ == "__main__":
-	main()
+	unittest.main()
 
 
 
